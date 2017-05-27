4fca52308c14cba0b108b95c68450f0a5e3e7166
chenqt <chenqt@ucweb.com>
2017-05-27 15:44:53 +0800

    修改支持url参数解析

--- apache/AccessLog.py
+++ apache/AccessLog.py
@@ -1,24 +1,26 @@
 import fileinput
 import re
 import os
+import urlparse
+import time
 try: import simplejson as json
 except ImportError: import json
 
 #read input file and return entries' Dict Object
 def readfile(file):
-    filecontent = {}
+    filecontent = []
     index = 0
     #check necessary file size checking
     statinfo = os.stat(file)
 
     #just a guestimate. I believe a single entry contains atleast 150 chars
     if statinfo.st_size < 150:
-        print "Not a valid access_log file. It does not have enough data"
+        print ("Not a valid access_log file. It does not have enough data")
     else:
         for line in fileinput.input(file):
             index = index+1
             if line != "\n": #don't read newlines
-                filecontent[index] = line2dict(line)
+                filecontent.append(line2dict(line))
 
         fileinput.close()
     return filecontent
@@ -31,7 +33,9 @@ def line2dict(line):
     r'(?P<IDENTITY>\S+)',               # indent %l (unused)
     r'(?P<USER>\S+)',                   # user %u
     r'\[(?P<TIME>.+)\]',                # time %t
-    r'"(?P<REQUEST>.+)"',               # request "%r"
+    r'"(?P<METHOD>\S+)',                # method "%r"
+    r'(?P<URL>\S+)',                    # url "%r"
+    r'(?P<PROTOCAL>\S+)"',              # protocal "%r"
     r'(?P<STATUS>[0-9]+)',              # status %>s
     r'(?P<SIZE>\S+)',                   # size %b (careful, can be '-')
     r'"(?P<REFERER>.*)"',               # referer "%{Referer}i"
@@ -40,6 +44,27 @@ def line2dict(line):
     pattern = re.compile(r'\s+'.join(parts)+r'\s*\Z')
     m = pattern.match(line)
     res = m.groupdict()
+    # url parse: file_name?xxx=yy&zz=tt
+    newRes = toParseUrl(res)
+    formatRes = toFormat(newRes)
+
+    return formatRes
+
+def toParseUrl(res): 
+    url = res['URL']
+    rs = urlparse.urlparse(url)
+    queryDict = urlparse.parse_qs(rs.query)
+    for key in queryDict: 
+        res[key] = queryDict[key][0]
+
+    return res
+
+
+def toFormat(res): 
+    oldTimeString = res['TIME'][0:-6]
+    timeObject = time.strptime(oldTimeString, "%d/%b/%Y:%H:%M:%S")
+    newTimeString = time.strftime("%Y-%m-%d %H:%M:%S", timeObject)
+    res['TIME'] = newTimeString
     return res
 
 #to get jSon of entire Log
@@ -47,6 +72,9 @@ def line2dict(line):
 def toJson(file):
     #get dict object for each entry
     entries = readfile(file)
-    return json.JSONEncoder().encode(entries)
+    outputString = ""
+    for line in entries: 
+        outputString += json.JSONEncoder().encode(line) + "\n"
 
-    
+    return outputString
+
